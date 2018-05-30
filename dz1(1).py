24a84c245084f780595ad7a6f66e0c0bc993d979
OlegPalanskyy <39740197+OlegPalanskyy@users.noreply.github.com>
2018-05-30 23:02:53 +0300

    Add files via upload

---
+++ dz1(1).py
@@ -0,0 +1,19 @@
+"""
+Запрограммировать следующее выражение:
+ (a + b - f / a) + f * a * a - (a + b) Числа а, b, f вводятся с клавиатуры.
+Организовать пользовательский интерфейс, таким образом, чтобы было понятно,
+в каком порядке должны вводиться числа.
+Пример:
+1.Введите A: 1
+2.Введите В: 1
+3.Введите F: 1
+4.Результат: 0
+
+"""
+
+first_number = float(input('Введите а: '))
+second_number = float(input('Введите b: '))
+third_number = float(input('Введите f: '))
+result = ((first_number + second_number - third_number / first_number)
+          + third_number * first_number * first_number - (first_number + second_number))
+print('Результат =', result)
---
+++ dz1(4).py
@@ -0,0 +1,26 @@
+"""
+Программа запрашивает у пользователя строку и
+выводит ее по центру в таблице 20х3.
+Пример 1:
+Enter string:
+elias
+xxxxxxxxxxxxxxxxxx
+x      elias      x
+xxxxxxxxxxxxxxxxxx
+Пример 2
+Enter string:
+O
+xxxxxxxxxxxxxxxxxx
+x        O        x
+xxxxxxxxxxxxxxxxxx
+"""
+name = input('name: ')
+line_1 = '*' * 20
+len_line = len(line_1)
+len_name = len(name)
+line_2 = ' ' * int((len_line / 2) - (len_name / 2))
+
+print(line_1)
+print('*' + line_2 + name + line_2 + "*")
+print(line_1)
+
